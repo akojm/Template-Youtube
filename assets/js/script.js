7d14c782b006fd2f16419713702e59e740c19e8f
akojm <ako.achats@free.fr>
2022-04-07 13:53:43 +0200

    second librairie mise à jour

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,15 @@ window.onload = ()=>{
 
     const iconBar =document.querySelector("#icon-bar");
     const menu=document.querySelector("#menu");
+    const paginateLeft=document.querySelector("#paginate-left");
+    const paginateRigth=document.querySelector("#paginate-right");
+    const numberOne=document.querySelector("#one-paginate");
+    const numberTwo=document.querySelector("#two-paginate");
+    const librairieFirst=document.querySelector("#librairie-1")
+    const librairieSecond=document.querySelector("#librairie-2")
     
+    // menu //
+
     if (iconBar) {
         iconBar.addEventListener('click', ()=>{
           if(menu.style.display==="block"){
@@ -18,5 +26,36 @@ window.onload = ()=>{
                 menu.style.display="none";         
         })   
     }
+
+    // librairie //
+
+    if (paginateRigth) {
+      paginateRigth.addEventListener('click', ()=>{
+        if (librairieSecond.style.display==="block") {
+          librairieSecond.style.display="none";
+        } else {
+          librairieFirst.style.display="none";
+          librairieSecond.style.display="flex";
+          numberTwo.style.background="gray";
+          numberOne.style.background="red";
+        }
+      })
+      
+    } 
+    if (paginateLeft) {
+      paginateLeft.addEventListener('click', ()=>{
+        if (librairieFirst.style.display==="none") {
+          librairieFirst.style.display="block";
+        } else {
+          librairieFirst.style.display="block";
+          librairieFirst.style.display="flex";
+          librairieSecond.style.display="none";
+          numberOne.style.background="gray";
+          numberTwo.style.background="red";
+        }
+      })
+      
+    } 
+    
 
 }
